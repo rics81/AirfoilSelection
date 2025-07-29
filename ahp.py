784d66940b76856945d8263d7c4ec8eab5a0e044
rics81 <ricsandrini@gmail.com>
2025-07-29 09:44:43 -0300

    Normalização dos valores
    A normalização foi necessaria para evitar divisões por zero na aplicação do AHP. Além disso o AHP-G não processa valores negativos.
    Após a normalização dos valores os valores mínimos (zero) foram substituidos pelo menor valor da série.

--- ahp.py
+++ ahp.py
@@ -4,112 +4,33 @@ Created on Fri Jun 13 12:40:01 2025
 
 @author: Ricardo
 """
-dfAHP_G_data = pd.DataFrame(columns = ['name',
-                                       'r2_clMax',
-                                       'r2_clStdRate',
-                                       'r2_clStdStallRate',
-                                       'r2_clMeanStallRate',
-                                       'r2_cdMax',
-                                       'r2_cmAlphaZero',
-                                       'r2_max cl/cd',
-                                       'r5_clMax',
-                                       'r5_clStdRate',
-                                       'r5_clStdStallRate',
-                                       'r5_clMeanStallRate',
-                                       'r5_cdMax',
-                                       'r5_cmAlphaZero',
-                                       'r5_max cl/cd',
-                                       'r10_clMax',
-                                       'r10_clStdRate',
-                                       'r10_clStdStallRate',
-                                       'r10_clMeanStallRate',
-                                       'r10_cdMax',
-                                       'r10_cmAlphaZero',
-                                       'r10_max cl/cd'])
-
-df = {}
-for airfoil in main_data:
-    print(airfoil['name'])
-   
-    if airfoil['name'] not in failed_airfoils['name'].values:
-        df.update({'name': airfoil['name']})
-        
-        for reynolds in ['200000', '500000', '1000000']:
-            # if airfoil.get(reynolds) is None:
-            #     failed_airfoils.append(airfoil['name'])
-            #     print('There is no reynolds')
-                
-            # else:
-            match reynolds:
-                case '200000':
-                    axColName = 'r2_'
-                case '500000':
-                    axColName = 'r5_'
-                case _:
-                    axColName = 'r10_'
-                    
-            # axColName = 'n5_' if reynolds == '200000-n5' else 'n9_'
-            
-            df.update({axColName + 'clMax': airfoil[reynolds]['clMax'],
-                       axColName + 'clStdRate': airfoil[reynolds]['clStdRate'],
-                       axColName + 'clStdStallRate': airfoil[reynolds]['clStdStallRate'],
-                       axColName + 'clMeanStallRate': airfoil[reynolds]['clMeanStallRate'],
-                       axColName + 'cdMax': airfoil[reynolds]['cdMax'],
-                       axColName + 'cmAlphaZero': airfoil[reynolds]['cmAlphaZero'],
-                       axColName + 'max cl/cd': airfoil[reynolds]['max cl/cd']})
-    
-        dfAHP_G_data = pd.concat([dfAHP_G_data, pd.DataFrame(df, index=[0])])
-    
-dfAHP_G_data['r2_max cl/cd'] = pd.to_numeric(dfAHP_G_data['r2_max cl/cd'])
-dfAHP_G_data['r5_max cl/cd'] = pd.to_numeric(dfAHP_G_data['r5_max cl/cd'])
-dfAHP_G_data['r10_max cl/cd'] = pd.to_numeric(dfAHP_G_data['r10_max cl/cd'])
-
-failed_airfoils = pd.concat([failed_airfoils, pd.DataFrame({'name':dfAHP_G_data[dfAHP_G_data.isnull().any(axis=1)]['name'],'desc':'no stall data'})])
-
-# ----
-
-"""Prepara dados para análise AHP."""
-#df_norm = df.copy()
-df_norm = dfAHP_G_data[~dfAHP_G_data['name'].isin(failed_airfoils['name'])].copy()
-df_norm = df_norm.reset_index(drop=True)
+from sklearn import preprocessing as skl_pp
 
 # Inverte indicadores "quanto menor melhor"
-inversoes = ['r2_clStdRate', 'r2_clStdStallRate', 'r2_clMeanStallRate', 'r2_cdMax', 'r2_cmAlphaZero',
-             'r5_clStdRate', 'r5_clStdStallRate', 'r5_clMeanStallRate', 'r5_cdMax', 'r5_cmAlphaZero',
-             'r10_clStdRate', 'r10_clStdStallRate', 'r10_clMeanStallRate', 'r10_cdMax', 'r10_cmAlphaZero',]
+inversoes = dfAirfoilFeatL.columns
 
-adj_value = df_norm.loc[df_norm['r2_clStdStallRate'] != 0, 'r2_clStdStallRate'].min()
-df_norm.loc[df_norm['r2_clStdStallRate'] == 0, 'r2_clStdStallRate'] = adj_value
-adj_value = df_norm.loc[df_norm['r5_clStdStallRate'] != 0, 'r5_clStdStallRate'].min()
-df_norm.loc[df_norm['r5_clStdStallRate'] == 0, 'r5_clStdStallRate'] = adj_value
-adj_value = df_norm.loc[df_norm['r10_clStdStallRate'] != 0, 'r10_clStdStallRate'].min()
-df_norm.loc[df_norm['r10_clStdStallRate'] == 0, 'r10_clStdStallRate'] = adj_value
+dfAHP = dfAirfoilFeat.copy()
 
-df_norm['r2_cmAlphaZero'] += 1
-df_norm['r5_cmAlphaZero'] += 1
-df_norm['r10_cmAlphaZero'] += 1
+fnScaler=skl_pp.MinMaxScaler()
+dfAHP=pd.DataFrame(fnScaler.fit_transform(dfAHP), columns=dfAHP.columns, index=dfAHP.index)
 
-df_norm['r2_clMeanStallRate'] += 1
-df_norm['r5_clMeanStallRate'] += 1
-df_norm['r10_clMeanStallRate'] += 1
+for col in dfAHP.columns:
+    dfAHP[col][dfAHP[col]==0] = dfAHP[col][dfAHP[col]!=0].min()
 
 for col in inversoes:
-    if col in df_norm.columns:
-        df_norm[col] = 1 / df_norm[col]
+    dfAHP[col] = 1 / dfAHP[col]
 
 # Normaliza
-airfoils = df_norm['name']
-df_norm = df_norm.drop(['name'], axis=1)
-df_norm = df_norm.div(df_norm.sum())
+dfAHP = dfAHP.div(dfAHP.sum())
 
 # Calcula pesos AHP
-df_stats = pd.DataFrame({
-    'media': df_norm.mean(),
-    'desvio': df_norm.std(),
-    'cv': df_norm.std() / df_norm.mean()
+dfAHPStats = pd.DataFrame({
+    'media': dfAHP.mean(),
+    'desvio': dfAHP.std(),
+    'cv': dfAHP.std() / dfAHP.mean()
 })
-df_stats['peso'] = df_stats['cv'] / df_stats['cv'].sum()
+dfAHPStats['peso'] = dfAHPStats['cv'] / dfAHPStats['cv'].sum()
 
 # Aplica pesos
-df_resultado = pd.DataFrame({'name': airfoils})
-df_resultado['score_ahp'] = df_norm.dot(df_stats['peso'])
+dfAHPRes = pd.DataFrame({'name': dfAirfoilFeat_names})
+dfAHPRes['score_ahp'] = dfAHP.dot(dfAHPStats['peso'])
